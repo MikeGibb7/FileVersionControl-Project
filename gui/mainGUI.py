e2a2bb796a2cae550e3529a43d738a7ead4836c3
TheSupremeXtream <85042240+TheSupremeXtream@users.noreply.github.com>
2025-12-09 22:22:42 -0500

    New GUI Input Layout [110036557 Ronit Mahajan]

--- gui/mainGUI.py
+++ gui/mainGUI.py
@@ -1,10 +1,10 @@
 import tkinter as tk
 import os.path as path
 
-def InputGUI(frame, message, row):
-    tk.Label(frame, text=message).grid(row=row, column=0, sticky="w")
+def InputGUI(frame, message, col):
+    tk.Label(frame, text=message).grid(row=0, column=col, pady=(0,5))
     entry = tk.Entry(frame, width=30)
-    entry.grid(row=row, column=1, padx=5)
+    entry.grid(row=1, column=col, padx=10)
     return entry
 
 def ButtonGUI(oldEntry, newEntry, oldText, newText, error):
@@ -66,12 +66,14 @@ root.title("LHdiff")
 
 top = tk.Frame(root)
 top.pack(fill="x", padx=10, pady=10)
+top.grid_columnconfigure(0, weight=1)
+top.grid_columnconfigure(1, weight=1)
 
 oldEntry = InputGUI(top, "Enter the old file: ", 0)
 newEntry = InputGUI(top, "Enter the new file: ", 1)
 
 error = tk.Label(top, text="", fg="red")
-error.grid(row=2, column=0, columnspan=2, pady=(0,5))
+error.grid(row=3, column=0, columnspan=2)
 
 bottom = tk.Frame(root)
 bottom.pack(fill="both", expand=True)
@@ -90,6 +92,10 @@ oldText.pack(side="left", fill="both", expand=True)
 newText = tk.Text(right, wrap="word")
 newText.pack(side="right", fill="both", expand=True)
 
-tk.Button(top, text="Enter", command=lambda:ButtonGUI(oldEntry, newEntry, oldText, newText, error)).grid(row=3, column=0, columnspan=2, pady=5)
+tk.Button(
+    top, 
+    text="Enter", 
+    command=lambda:ButtonGUI(oldEntry, newEntry, oldText, newText, error)
+).grid(row=2, column=0, columnspan=2, pady=10)
 
 root.mainloop()
