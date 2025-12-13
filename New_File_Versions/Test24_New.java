afeb4d900c29132811b0fe1d9ba51eefaa266116
Nico Belanger <belang13@uwindsor.ca>
2025-12-13 12:21:38 -0500

    Test24

---
+++ New_File_Versions/Test24_New.java
@@ -0,0 +1,108 @@
+public class Test24 {
+
+    private int counter;
+    private String name;
+
+    public Test24(String name) {
+        this.name = name;
+        this.counter = 0;
+    }
+
+    public void start() {
+        initialize();
+        process();
+        finish();
+    }
+
+    private void initialize() {
+        System.out.println("Initializing...");
+        counter = 1;
+        logState();
+    }
+
+    private void process() {
+        for (int i = 0; i < 5; i++) {
+            counter += i;
+            updateStatus(i);
+            validate(counter);
+        }
+    }
+
+    private void finish() {
+        cleanup();
+        System.out.println("Done");
+    }
+
+    private void updateStatus(int step) {
+        System.out.println("Step: " + step);
+        saveProgress(step);
+    }
+
+    private void saveProgress(int value) {
+        if (value % 2 == 0) {
+            System.out.println("Saving even step");
+        } else {
+            System.out.println("Skipping odd step");
+        }
+    }
+
+    private void validate(int value) {
+        if (value < 0) {
+            throw new IllegalStateException("Invalid value");
+        }
+    }
+
+    private void cleanup() {
+        System.out.println("Cleaning resources");
+        reset();
+    }
+
+    private void reset() {
+        counter = 0;
+    }
+
+    private void logState() {
+        System.out.println("Counter = " + counter);
+        System.out.println("Name = " + name);
+    }
+
+    private void unusedMethodB() {
+        System.out.println("Unused B");
+    }
+
+    private void unusedMethodC() {
+        System.out.println("Unused C");
+    }
+
+    private void helperOne() {
+        System.out.println("Helper One");
+    }
+
+    private void helperTwo() {
+        System.out.println("Helper Two");
+    }
+
+    private void helperThree() {
+        System.out.println("Helper Three");
+    }
+
+    private void helperEleven() {
+        System.out.println("Helper Eleven");
+    }
+
+     private void helperFour() {
+        System.out.println("Helper Four");
+    }
+
+    private void helperEight() {
+        System.out.println("Helper Eight");
+    }
+
+    private void helperNine() {
+        System.out.println("Helper Nine");
+    }
+
+    private void helperTen() {
+        System.out.println("Helper Ten");
+    }
+}
