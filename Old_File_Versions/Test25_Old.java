9f3f4c309e155f13d0d2927e5ce2fc8fcc6f0c86
Nico Belanger <belang13@uwindsor.ca>
2025-12-13 12:31:48 -0500

    Test25

---
+++ Old_File_Versions/Test25_Old.java
@@ -0,0 +1,76 @@
+public class TaskManager {
+
+    private String name;
+    private int maxTasks;
+    private int currentTasks;
+
+    public TaskManager(String name, int maxTasks) {
+        this.name = name;
+        this.maxTasks = maxTasks;
+        this.currentTasks = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMaxTasks() {
+        return maxTasks;
+    }
+
+    public int getCurrentTasks() {
+        return currentTasks;
+    }
+
+    public boolean canAddTask() {
+        return currentTasks < maxTasks;
+    }
+
+    public void addTask() {
+        if (canAddTask()) {
+            currentTasks++;
+        }
+    }
+
+    public void removeTask() {
+        if (currentTasks > 0) {
+            currentTasks--;
+        }
+    }
+
+    public void resetTasks() {
+        currentTasks = 0;
+    }
+
+    public void printStatus() {
+        System.out.println("Manager: " + name);
+        System.out.println("Tasks: " + currentTasks + "/" + maxTasks);
+    }
+
+    public void simulateWorkload() {
+        addTask();
+        addTask();
+        removeTask();
+        addTask();
+        printStatus();
+    }
+
+    public void logStart() {
+        System.out.println("Starting task manager...");
+    }
+
+    public void logEnd() {
+        System.out.println("Stopping task manager...");
+    }
+
+    public void fullRun() {
+        logStart();
+        simulateWorkload();
+        logEnd();
+    }
+
+    public static void main(String[] args) {
+        TaskManager tm = new TaskManager("Primary", 5);
+        tm.fullRun();
+    }
+}
